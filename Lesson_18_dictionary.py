16adf25ad7c94cd52850f472a0f65d7af36020d4
Levente Blanár <blanar.levente@gmail.com>
2025-01-29 15:13:24 +0100

    Update Lesson_18_dictionary.py

--- Lesson_18_dictionary.py
+++ Lesson_18_dictionary.py
@@ -6,4 +6,26 @@ capitals = {"USA": "Washington D.C",
             "China": "Beijing",
             "Russai": "Moscow"}
 
-print(dir(capitals))
+# print(dir(capitals))
+# print(help(capitals))
+
+capitals.get("USA")
+
+if capitals.get("Japan"):
+    print("That capital exists")
+else:
+    print("That capital doesn't exist")
+
+
+
+
+# capitals.update({"Germany": "Berlin"}) - can insert a new 
+# capitals.update({"USA:": "Detroit"}) - or update an already existing one
+# capitals.pop("China") - remove a key value pair
+# capitals.popitem() - remove the latest one
+# capitals.clear() - clears the dictionary
+# capitals.keys() - get all of the keys but not the values
+# capitals.values() - get only the values
+# capitals.items() - returns a dictionary object that resembles a 2d list tuple
+
+print(capitals)
