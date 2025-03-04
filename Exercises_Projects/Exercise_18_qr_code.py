3334184d1f0c4b27de4e1a455b10202f4ed53d52
Levente Blanár <blanar.levente@gmail.com>
2025-03-04 16:39:34 +0100

    Little mod on size

--- Exercises_Projects/Exercise_18_qr_code.py
+++ Exercises_Projects/Exercise_18_qr_code.py
@@ -5,7 +5,7 @@ from PIL import Image
 # img = qrcode.make("https://github.com/leventeblanar")
 # img.save("levente_blanar_GH.png")
 
-qr = qrcode.QRCode(version=1, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size= 40, border = 10 )
+qr = qrcode.QRCode(version=1, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size= 40, border = 5 )
 qr.add_data("https://github.com/leventeblanar")
 qr.make(fit=True)
 img=qr.make_image(fill_color = "lightgreen", back_color="purple")
