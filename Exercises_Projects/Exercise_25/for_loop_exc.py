c16a45a3431614898f1e9e4800aee7dfd3493296
Levente Blanár <blanar.levente@gmail.com>
2025-03-27 16:19:57 +0100

    Update for_loop_exc.py

--- Exercises_Projects/Exercise_25/for_loop_exc.py
+++ Exercises_Projects/Exercise_25/for_loop_exc.py
@@ -49,5 +49,35 @@ def random_number():
         print("Sajnálo, elfogyott a lehetőséged! A szám:", random_number)
 
 
+def duouble_value():
+    list = [1, 3, 5, 7, 9]
+
+    for element in list:
+        print(element * 2)
+
+
+def square():
+    numbers = [2, 4, 6, 8, 10]
+
+    for element in numbers:
+        print(element ** 2)
+
+
+def generate_index():
+    split_text = "python"
+
+    for index, letter in enumerate(split_text):
+        print(f"{index + 1}.", letter)
+
+
+def find_vowels():
+    vowels = ["a","e","i","o","u"]
+
+    user_input = input("Adj egy szöveget: ")
+
+    for letter in user_input:
+        if letter in vowels:
+            print(letter)
+
 if __name__ == '__main__':
-    random_number()
+    find_vowels()
