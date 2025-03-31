13d0cf32b936d22f3bf958dbb17fb63dbe3e6146
lblanar <lblanar@dataedge.io>
2025-03-31 09:38:53 +0200

    Update on reading the Automate the boring stuff book

--- Exercises_Projects/Exercise_27_list_addition.py
+++ Exercises_Projects/Exercise_27_list_addition.py
@@ -1,12 +1,19 @@
-dogNames = []
+# dogNames = []
 
-while True:
-    print('Enter the name of cat ' + str(len(dogNames) + 1) +
-      ' (Or enter nothing to stop.):')
-    name = input()
-    if name == '':
-        break
-    dogNames = dogNames + [name]
-    print('The dof names are:')
-    for name in dogNames:
-        print(' ' + name)
+# while True:
+#     print('Enter the name of cat ' + str(len(dogNames) + 1) +
+#       ' (Or enter nothing to stop.):')
+#     name = input()
+#     if name == '':
+#         break
+#     dogNames = dogNames + [name]
+#     print('The dof names are:')
+#     for name in dogNames:
+#         print(' ' + name)
+
+
+
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+
+for i in range(len(supplies)):
+    print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
