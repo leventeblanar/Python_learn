4fe59e5d2639831270728ce965de08a4be899063
lblanar <blanar.levente@gmail.com>
2025-04-11 10:07:19 +0200

    Update sql_scripts.py

--- Standard_scripts/sql_scripts.py
+++ Standard_scripts/sql_scripts.py
@@ -1,16 +1,29 @@
 import pandas as pd
 
-def get_data_from_excel():
+def get_data_from_excel_forSQL():
 
-    excel = 'file_source'
+    ## This is a simple method of turning a bunch of excel data into SQL prompts line by line
 
-    excel_df = pd.read_excel(excel)
+    excel = 'file_path'
 
-    # dropping Nan values
-    excel_df_notnan = excel_df.dropna()
-    excel_df_notnan = excel_df_notnan.astype({'col1': int, 'col2': int})
+    ### This is a converter method to clean data before even reading xlsx files
+    def clean_float_as_str(val):
+        if pd.isna(val):  # if the value is Nan - keep it as Nan
+            return val
+        elif isinstance(val, float) and val.is_integer(): # if the value is float but the value itself is an integer (1.0)
+            return str(int(val)) # return a string made of the float 1.0 converted to int 1 -> '1'
+        return str(val)
     
-    for index, row in excel_df_notnan.iterrows():
+    excel_df = pd.read_excel(excel, converters={
+        "col1": clean_float_as_str,
+        "col2": clean_float_as_str
+    })
+
+    for index, row in excel_df.iterrows():
         print(row['col1'], row['col2'])
-        with open('output.sql', 'a') as sql_file:
-            sql_file.write(f"SQL_SCRIPT")
+        with open('output.sql', 'w') as sql_file:
+            if pd.isna(row['col2']):
+                sql_file.write(f"SQL_1")
+            else:
+                sql_file.write(f"SQL_2")
+
