a0b407fbd5ff6b709a941ee0859299b57c6d9a07
lblanar <blanar.levente@gmail.com>
2025-05-08 15:50:49 +0200

    practice

--- Exercises_Projects/Exercise_29/check_client.py
+++ Exercises_Projects/Exercise_29/check_client.py
@@ -1,10 +1,34 @@
 import pandas as pd
 import json
 
+def validate_partner(partner_dict):
+    
+    # This is to validate info on given partner
+
+    ervenytelen_partnerek = []
+
+    required_fields = ["name", "zip", "city", "address"]
+
+    for partner in partner_dict:
+        hibas = False
+        print("-------------------")
+
+        for field in required_fields:
+            value = partner.get(field, "")
+            print(f"{field.capitalize()}: {value}")
+            if not value:
+                print(f"Hiányzó {field} mező.")
+                hibas = True
+        
+        if hibas:
+            ervenytelen_partnerek.append(partner['name'])
+        
+    print("\nÉrvénytelen partnerek száma: ", len(ervenytelen_partnerek))
+    print("\nÉrvénytelen partnerek: ", ervenytelen_partnerek)
+
+
+
 with open('client_list.json', "r", encoding='utf-8') as file:
     partner_dict = json.load(file)
 
-print(partner_dict)
-
-def validate_partner():
-    pass
+validate_partner(partner_dict)
